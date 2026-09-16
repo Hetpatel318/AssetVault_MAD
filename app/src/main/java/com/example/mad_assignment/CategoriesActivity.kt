8a32872bb8e45a85b0965afca1bad99842ea9e4c
Het <hetpatel0831@gmail.com>
2026-09-16 23:32:43 +0530

    Add categories activity

---
+++ app/src/main/java/com/example/mad_assignment/CategoriesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mad_assignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CategoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories)
+    }
+}
